95a94bbd8c0f71497e3285f1505a823eafaf6037
spara <sophia.parafina@gmail.com>
2020-03-10 18:15:41 -0500

    added eks

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+import * as eks from "@pulumi/eks";
+import * as k8s from "@pulumi/kubernetes";
 
 const builderRole = new aws.iam.Role('builder-role', {
     assumeRolePolicy: {
@@ -106,19 +109,104 @@ const buildProject = new aws.codebuild.Project("aws-codebuild", {
 });
 
 new aws.codebuild.Webhook('aws-codebuild-webhook', {
-  projectName: buildProject.name,
-  filterGroups: [
-    {
-      filters: [
-        {
-          type: 'EVENT',
-          pattern: 'PUSH',
-        },
-        {
-          type: 'HEAD_REF',
-          pattern: 'refs/heads/master',
-        },
-      ],
-    },
-  ],
+    projectName: buildProject.name,
+    filterGroups: [
+      {
+        filters: [
+            {
+                "type": "EVENT", 
+                "pattern": "PULL_REQUEST_CREATED, PULL_REQUEST_UPDATED, PULL_REQUEST_REOPENED"
+            },
+            {
+                "type": "HEAD_REF", 
+                "pattern": "^refs/heads/myBranch$"
+            },
+            {
+                "type": "BASE_REF", 
+                "pattern": "^refs/heads/master$"
+            }
+        ],
+      },
+    ],
+  });
+
+  const name = "helloworld";
+
+// Create an EKS cluster with non-default configuration
+const vpc = new awsx.ec2.Vpc("vpc", { numberOfAvailabilityZones: 2 });
+
+const cluster = new eks.Cluster(name, {
+    vpcId: vpc.id,
+    subnetIds: vpc.publicSubnetIds,
+    desiredCapacity: 2,
+    minSize: 1,
+    maxSize: 2,
+    storageClasses: "gp2",
+    deployDashboard: false,
 });
+
+// Export the clusters' kubeconfig.
+export const kubeconfig = cluster.kubeconfig;
+
+// Create a Kubernetes Namespace
+const ns = new k8s.core.v1.Namespace(name, {}, { provider: cluster.provider });
+
+// Export the Namespace name
+export const namespaceName = ns.metadata.name;
+
+// Create a NGINX Deployment
+const appLabels = { appClass: name };
+const deployment = new k8s.apps.v1.Deployment(name,
+    {
+        metadata: {
+            namespace: namespaceName,
+            labels: appLabels,
+        },
+        spec: {
+            replicas: 1,
+            selector: { matchLabels: appLabels },
+            template: {
+                metadata: {
+                    labels: appLabels,
+                },
+                spec: {
+                    containers: [
+                        {
+                            name: name,
+                            image: "nginx:latest",
+                            ports: [{ name: "http", containerPort: 80 }],
+                        },
+                    ],
+                },
+            },
+        },
+    },
+    {
+        provider: cluster.provider,
+    },
+);
+
+// Export the Deployment name
+export const deploymentName = deployment.metadata.name;
+
+// Create a LoadBalancer Service for the NGINX Deployment
+const service = new k8s.core.v1.Service(name,
+    {
+        metadata: {
+            labels: appLabels,
+            namespace: namespaceName,
+        },
+        spec: {
+            type: "LoadBalancer",
+            ports: [{ port: 80, targetPort: "http" }],
+            selector: appLabels,
+        },
+    },
+    {
+        provider: cluster.provider,
+    },
+);
+
+// Export the Service name and public LoadBalancer Endpoint
+export const serviceName = service.metadata.name;
+export const serviceHostname = service.status.loadBalancer.ingress[0].hostname;
